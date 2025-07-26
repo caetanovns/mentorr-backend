9644e5cf38b5a2513eb26ed1aaae3caa9f9f8467
caetanovns <caetanov120@gmail.com>
2025-07-26 17:55:20 -0300

    - adicionado imagem e jenkinsfile.

---
+++ entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sleep 2
+
+# Executa as migrations
+php artisan migrate --force
+
+# Executa os seeders
+php artisan db:seed --force
+
+chown -R www-data:www-data storage bootstrap/cache
+
+apache2-foreground
